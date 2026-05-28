1cfe07c417aec2fa3a4af2318d0ac6f65319684c
AlMizanBot <mdmnh394@gmail.com>
2026-05-28 23:53:35 +0300

    Create config.go

---
+++ pkg/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+    "os"
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    DatabaseURL string
+    Port        string
+}
+
+func LoadConfig() *Config {
+    godotenv.Load()
+    return &Config{
+        DatabaseURL: os.Getenv("DATABASE_URL"),
+        Port:        os.Getenv("PORT"),
+    }
+}
